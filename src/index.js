99772b0a7746eea4e2c04a2165ad657709c726c2
MD Sadid <sfhsadid@gmail.com>
2024-08-25 08:27:07 +0300

    app file add new middelwar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(
+    app.listen(process.env.PORT, () => {
+      console.log("Yiasemi Server is Listening on Port: ", process.env.PORT);
+    })
+  )
+  .catch((err) => console.log(`The database connition fiald ${err}`));
