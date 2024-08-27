0df7b35fae10d765026787d6fa01708643381c55
MD Sadid <sfhsadid@gmail.com>
2024-08-27 09:54:04 +0300

    generateAccesstoken create done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -42,5 +42,18 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {};
 
 export const User = mongoose.model("User", userSchema);
