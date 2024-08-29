e411f998409ba7067009d0cea80b0aa2a0aa4024
MD Sadid <sfhsadid@gmail.com>
2024-08-29 10:20:53 +0300

    user controller 10% done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,22 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "nice",
-  });
+  // get user details form frontend
+  // validation - not empty
+  // check if user already exists? userName, email
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return respons
+
+  const { userName, email, password, avatar } = req.body;
+  if (
+    [userName, email, password, avatar].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are requied");
+  }
 });
 
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router();
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.single([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
