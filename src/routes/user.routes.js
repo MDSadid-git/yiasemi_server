4654066c5165c96922e08173fcb3748c34ce9b37
MD Sadid <sfhsadid@gmail.com>
2024-09-03 08:28:24 +0300

    user route done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import {
+  accountDetailsUpdate,
+  changeCurrentPassword,
+  getCurrentUser,
   loginUser,
   logOutUser,
   refreshAccessToken,
   registerUser,
+  userAvatarUpdate,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewars.js";
@@ -22,4 +26,10 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, accountDetailsUpdate);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), userAvatarUpdate);
 export default router;
