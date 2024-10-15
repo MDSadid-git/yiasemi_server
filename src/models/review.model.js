cc2e9ce5c21180c3717cf281e1f0e107aabed3b9
MD Sadid <sfhsadid@gmail.com>
2024-10-15 08:39:31 +0300

    review model create done

---
+++ src/models/review.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const reviewSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    details: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Review = mongoose.model("review", reviewSchema);
