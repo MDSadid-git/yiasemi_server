eac010622a40dacdb8c4d4646c29c738b8f950e0
MD Sadid <sfhsadid@gmail.com>
2024-10-17 09:03:52 +0300

    add review create done

---
+++ src/controllers/review.controller.js
@@ -0,0 +1,26 @@
+import { Review } from "../models/review.model.js";
+import { ApiResponse } from "../utils/ApiRespose.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const addReviewsUser = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  const { name, details, rating } = req.body;
+  if ([name, details].some((field) => field?.trim() === "")) {
+    return res
+      .status(400)
+      .json(new ApiResponse(400, `All fields are requied`, "Faild"));
+  }
+  if (rating == 0) {
+    return res
+      .status(400)
+      .json(new ApiResponse(400, `All fields are requied`, "Faild"));
+  }
+  const addReview = await Review.create({
+    name,
+    details,
+    rating,
+  });
+  return res.status(200).json(new ApiResponse(200, addReview, "Successfull"));
+});
+
+export { addReviewsUser };
--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middlewars.js";
+import { addReviewsUser } from "../controllers/review.controller.js";
 
 const router = Router();
 
-router.route("/add-reviews").post();
+router.route("/add-reviews").post(verifyJWT, addReviewsUser);
 
 export default router;
