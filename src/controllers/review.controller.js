7154d62d79b65f082c6e26541cb672eaa11fc730
MD Sadid <sfhsadid@gmail.com>
2024-10-22 08:29:45 +0300

    user Review List

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -2,9 +2,21 @@ import { Review } from "../models/review.model.js";
 import { ApiResponse } from "../utils/ApiRespose.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const allReviews = asyncHandler(async (req, res) => {
+const allReviews = asyncHandler(async (_, res) => {
   const reviews = await Review.find({});
   return res.status(200).json(new ApiResponse(200, reviews, "Successfull"));
+});
+
+const userReviewsList = asyncHandler(async (req, res) => {
+  const name = req.user.userName;
+  const result = await Review.find({ name });
+
+  if (!result) {
+    return res
+      .status(500)
+      .json(new ApiResponse(500, "This user add on comment", "Faild"));
+  }
+  return res.status(200).json(new ApiResponse(200, result, "Successfull"));
 });
 
 const addReviewsUser = asyncHandler(async (req, res) => {
@@ -28,4 +40,4 @@ const addReviewsUser = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, addReview, "Successfull"));
 });
 
-export { addReviewsUser, allReviews };
+export { addReviewsUser, allReviews, userReviewsList };
--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -3,11 +3,13 @@ import { verifyJWT } from "../middlewares/auth.middlewars.js";
 import {
   addReviewsUser,
   allReviews,
+  userReviewsList,
 } from "../controllers/review.controller.js";
 
 const router = Router();
 
 router.route("/all-reviews").get(allReviews);
+router.route("/user-all-reviews").get(verifyJWT, userReviewsList);
 router.route("/add-reviews").post(verifyJWT, addReviewsUser);
 
 export default router;
